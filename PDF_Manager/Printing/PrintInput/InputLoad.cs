207ca87b5b1c21f46611ea9b4ce805b56fde1ed4
ササザワヨウスケ <sasaicco@gmail.com>
2022-01-13 13:19:14 +0900

    荷重L1 が null の場合のエラー修正

--- PDF_Manager/Printing/PrintInput/InputLoad.cs
+++ PDF_Manager/Printing/PrintInput/InputLoad.cs
@@ -56,9 +56,26 @@ namespace PDF_Manager.Printing
                             line[0] = mc.TypeChange(member["m1"]);
                             line[1] = mc.TypeChange(member["m2"]);
                             line[2] = mc.TypeChange(member["direction"]);
-                            line[3] = mc.TypeChange(member["mark"]); 
-                            line[4] = member["L1"].ToString().StartsWith("-0") ? "-0.000" : mc.TypeChange(double.Parse(member["L1"].ToString()),3);
-                            line[5] = mc.TypeChange(member["L2"],3);
+                            line[3] = mc.TypeChange(member["mark"]);
+
+                            // line[4] = member["L1"].ToString().StartsWith("-0") ? "-0.000" : mc.TypeChange(double.Parse(member["L1"].ToString()), 3);
+                            var a4 = member["L1"].ToString();
+                            if(a4.Trim().Length == 0)
+                            {
+                                line[4] = "";
+                            } else {
+                                var b4 = double.Parse(a4);
+                                if (b4 == 0 && a4.StartsWith("-"))
+                                {
+                                    line[4] = "-0.000"; // ゼロにマイナスがついている場合
+                                }
+                                else
+                                {
+                                    line[4] = mc.TypeChange(b4, 3);
+                                }
+                            }
+
+                            line[5] = mc.TypeChange(member["L2"], 3);
                             line[6] = mc.TypeChange(member["P1"], 2);
                             line[7] = mc.TypeChange(member["P2"], 2);
                             table1.Add(line);
