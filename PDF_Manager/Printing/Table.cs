d2a0c3dd657383d1f9744ff66a4804286206a9a6
sasaco <sasaicco@gmail.com>
2022-05-12 20:11:33 +0900

    er

--- PDF_Manager/Printing/Table.cs
+++ PDF_Manager/Printing/Table.cs
@@ -12,46 +12,46 @@ namespace PDF_Manager.Printing
     {
         #region This Class Palamator
         /*
-         * Cols=5 →       1             2             3             4             5...
+         * Cols=5 →       0             1             2             3             4...
          * Rows=4
          * ↓     Point[0, 0]   Point[0, 1]   Point[0, 2]   Point[0, 3]   Point[0, 4]   Point[0, 5]
          *           ・──────・──────・──────・──────・──────・
-         *  1        │  Cell[1,1] │  Cell[1,2] │  Cell[1,3] │  Cell[1,4] │  Cell[1,5] │
-         *           │ Align[1,1] │ Align[1,2] │ Align[1,3] │ Align[1,4] │ Align[1,5] │　　　　
+         *  0        │  Cell[0,0] │  Cell[0,1] │  Cell[0,2] │  Cell[0,3] │  Cell[0,4] │
+         *           │ Align[0,0] │ Align[0,1] │ Align[0,2] │ Align[0,3] │ Align[0,4] │　　　　
          *           ├──────┼──────┼──────┼──────┼──────┤
-         *  2        │  Cell[2,1] │  Cell[2,2] │  Cell[2,3] │  Cell[2,4] │  Cell[2,5] │
-         *           │ Align[2,1] │ Align[2,2] │ Align[2,3] │ Align[2,4] │ Align[2,5] │　　　　
+         *  1        │  Cell[1,0] │  Cell[1,1] │  Cell[1,2] │  Cell[1,3] │  Cell[1,4] │
+         *           │ Align[1,0] │ Align[1,1] │ Align[1,2] │ Align[1,3] │ Align[1,4] │　　　　
          *           ├──────┼──────┼──────┼──────┼──────┤
-         *  3        │  Cell[3,1] │  Cell[3,2] │  Cell[3,3] │  Cell[3,4] │  Cell[3,5] │
-         *           │ Align[3,1] │ Align[3,2] │ Align[3,3] │ Align[3,4] │ Align[3,5] │　　　　
+         *  2        │  Cell[2,0] │  Cell[2,1] │  Cell[2,2] │  Cell[2,3] │  Cell[2,4] │
+         *           │ Align[2,0] │ Align[2,1] │ Align[2,2] │ Align[2,3] │ Align[2,4] │　　　　
          *           ├──────┼──────┼──────┼──────┼──────┤
-         *  4        │  Cell[4,1] │  Cell[4,2] │  Cell[4,3] │  Cell[4,4] │  Cell[4,5] │
-         *  :        │ Align[4,1] │ Align[4,2] │ Align[4,3] │ Align[4,4] │ Align[4,5] │　　　　
+         *  3        │  Cell[3,0] │  Cell[3,1] │  Cell[3,2] │  Cell[3,3] │  Cell[3,4] │
+         *  :        │ Align[3,0] │ Align[3,1] │ Align[3,2] │ Align[3,3] │ Align[3,4] │　　　　
          *           ・──────・──────・──────・──────・──────・
          *        Point[4, 0]   Point[4, 1]   Point[4, 2]   Point[4, 3]   Point[4, 4]   Point[4, 5]
          *                                                                              
          *
-         *           ┌-HolLW[0,1]-┬-HolLW[0,2]-┬-HolLW[0,3]-┬-HolLW[0,4]-┬-HolLW[0,5]-┐
+         *           ┌-HolLW[0,0]-┬-HolLW[0,1]-┬-HolLW[0,2]-┬-HolLW[0,3]-┬-HolLW[0,4]-┐
+         *           │            │            │            │            │            │　　　　
+         * VtcLW[0,0]┥  VtcLW[0,1]┥  VtcLW[0,2]┥  VtcLW[0,3]┥  VtcLW[0,4]┥  VtcLW[0,5]┥　RowHeight[0] 　　　　
+         *           │            │            │            │            │            │　　　　
+         *           ├-HolLW[1,0]-┼-HolLW[1,1]-┼-HolLW[1,2]-┼-HolLW[1,3]-┼-HolLW[1,4]-┤
          *           │            │            │            │            │            │　　　　
          * VtcLW[1,0]┥  VtcLW[1,1]┥  VtcLW[1,2]┥  VtcLW[1,3]┥  VtcLW[1,4]┥  VtcLW[1,5]┥　RowHeight[1] 　　　　
          *           │            │            │            │            │            │　　　　
-         *           ├-HolLW[1,1]-┼-HolLW[1,2]-┼-HolLW[1,3]-┼-HolLW[1,4]-┼-HolLW[1,5]-┤
+         *           ├-HolLW[2,0]-┼-HolLW[2,1]-┼-HolLW[2,2]-┼-HolLW[2,3]-┼-HolLW[2,4]-┤
          *           │            │            │            │            │            │　　　　
          * VtcLW[2,0]┥  VtcLW[2,1]┥  VtcLW[2,2]┥  VtcLW[2,3]┥  VtcLW[2,4]┥  VtcLW[2,5]┥　RowHeight[2] 　　　　
          *           │            │            │            │            │            │　　　　
-         *           ├-HolLW[2,1]-┼-HolLW[2,2]-┼-HolLW[2,3]-┼-HolLW[2,4]-┼-HolLW[2,5]-┤
+         *           ├-HolLW[3,0]-┼-HolLW[3,1]-┼-HolLW[3,2]-┼-HolLW[3,3]-┼-HolLW[3,4]-┤
          *           │            │            │            │            │            │　　　　
-         * VtcLW[3,0]┥  VtcLW[3,1]┥  VtcLW[3,2]┥  VtcLW[3,3]┥  VtcLW[3,4]┥  VtcLW[3,5]┥　RowHeight[3] 　　　　
+         * VtcLW[3,0]┥  VtcLW[3,1]┥  VtcLW[3,2]┥  VtcLW[3,3]┥  VtcLW[3,4]┥  VtcLW[3,5]┥　RowHeight[3]　　　　
          *           │            │            │            │            │            │　　　　
-         *           ├-HolLW[3,1]-┼-HolLW[3,2]-┼-HolLW[3,3]-┼-HolLW[3,4]-┼-HolLW[3,5]-┤
-         *           │            │            │            │            │            │　　　　
-         * VtcLW[4,0]┥  VtcLW[4,1]┥  VtcLW[4,2]┥  VtcLW[4,3]┥  VtcLW[4,4]┥  VtcLW[4,5]┥　RowHeight[4]　　　　
-         *           │            │            │            │            │            │　　　　
-         *           └-HolLW[4,1]-┴-HolLW[4,2]-┴-HolLW[4,3]-┴-HolLW[4,4]-┴-HolLW[4,4]-┘
+         *           └-HolLW[4,0]-┴-HolLW[4,1]-┴-HolLW[4,2]-┴-HolLW[4,3]-┴-HolLW[4,4]-┘
          * 
          *           │            │            │            │            │            │　　　　
          *           └──────┴──────┴──────┴──────┴──────┘
-         *              ColWidth[1]   ColWidth[2]   ColWidth[3]   ColWidth[4]   ColWidth[5]
+         *              ColWidth[0]   ColWidth[1]   ColWidth[2]   ColWidth[3]   ColWidth[4]
          */
         #endregion
 
@@ -72,21 +72,14 @@ namespace PDF_Manager.Printing
         {
 
         }
-        public Table(int _rows, int _cols)
-        {
-            //** Row, Col Count
-            this.CellRows = _rows;
-            this.CellCols = _cols;
 
-            this.setRowCount(_rows);
-        }
-
-        public void setRowCount(int _rows)
+        public void setRowCount(int _rows, int _cols)
         {
             // 昔の情報を取っておく
-            var oldCell = new string[_rows + 1, this.CellCols + 1];
-            var oldAlignX = new string[_rows + 1, this.CellCols + 1];
-            var oldAlignY = new string[_rows + 1, this.CellCols + 1];
+            var oldCell   = new string[_rows, _cols];
+            var oldAlignX = new string[_rows, _cols];
+            var oldAlignY = new string[_rows, _cols];
+
             if (this.Cell != null)
             {
                 oldCell = this.Cell.Clone() as string[,];
@@ -94,60 +87,36 @@ namespace PDF_Manager.Printing
                 oldAlignY = this.AlignY.Clone() as string[,];
             }
             int oldRows = this.CellRows; // 昔のデータの行数
-
-            //** Row, Col Count
-            this.CellRows = _rows;
+            int oldCols = this.CellCols; // 昔のデータの列数
 
             //** Init Cell
-            this.Cell = new string[_rows + 1, this.CellCols + 1];
-            for (int j = 1; j <= this.CellCols; ++j)
-                for (int i = 1; i <= oldRows; ++i)
-                    this.Cell[i, j] = oldCell[i,j];
+            this.Cell = new string[_rows, _cols];
+            for (int j = 0; j < oldCols; ++j)
+                for (int i = 0; i < oldRows; ++i)
+                    this.Cell[i, j] = oldCell[i, j];
 
 
             //** Init Align
-            this.AlignX = new string[_rows + 1, this.CellCols + 1];
-            for (int j = 1; j <= this.CellCols; ++j) { 
-                for (int i = 1; i <= oldRows; ++i)
+            this.AlignX = new string[_rows, _cols];
+            for (int j = 0; j < oldCols; ++j) { 
+                for (int i = 0; i < oldRows; ++i)
                     this.AlignX[i, j] = oldAlignX[i, j];
-                for (int i = oldRows + 1; i <= _rows; ++i)
+                for (int i = oldRows; i < _rows; ++i)
                     this.AlignX[i, j] = "C";
             }
 
-            this.AlignY = new string[_rows + 1, this.CellCols + 1];
-            for (int j = 1; j <= this.CellCols; ++j)
-            {
-                for (int i = 1; i <= oldRows; ++i)
+            this.AlignY = new string[_rows, _cols];
+            for (int j = 0; j < oldCols; ++j)  {
+                for (int i = 0; i < oldRows; ++i)
                     this.AlignY[i, j] = oldAlignY[i, j];
-                for (int i = oldRows + 1; i <= _rows; ++i)
+                for (int i = oldRows; i < _rows; ++i)
                     this.AlignX[i, j] = "C";
             }
 
-            //** Init RowHeight
-            this.RowHeight = new double[_rows + 1];
-            this.RowHeight[0] = 0;
-            for (int i = 1; i <= _rows; ++i)
-                this.RowHeight[i] = -1;  // 0 > Fit Strings Size
+            //** Row, Col Count
+            this.CellRows = _rows;
+            this.CellCols = _cols;
 
-            //** Init ColWidth
-            this.ColWidth = new double[this.CellCols + 1];
-            this.ColWidth[0] = 0;
-            for (int j = 1; j <= this.CellCols; ++j)
-                this.ColWidth[j] = -1;   // 0 > Fit Strings Size
-
-            //** Init Holizonal Line Width
-            this.HolLW = new double[_rows + 1, this.CellCols + 1];
-            for (int i = 0; i <= _rows; ++i)
-                this.SetHolLW(i, 0.2F);
-            this.SetHolLW(0, 1);
-            this.SetHolLW(_rows, 1);
-
-            //** Init Holizonal Line Width
-            this.VtcLW = new double[_rows + 1, this.CellCols + 1];
-            for (int j = 0; j <= this.CellCols; ++j)
-                this.SetVtcLW(j, 0.2F);
-            this.SetVtcLW(0, 1);
-            this.SetVtcLW(this.CellCols, 1);
         }
 
         public string this[int row, int col]
